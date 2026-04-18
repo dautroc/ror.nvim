ed6556f8a3bee1e2a9288bacc6991c61ff665cff
Loi <ducloi221@gmail.com>
2026-04-18 20:02:53 +0700

    feat: rewrite test navigator with generic path resolution and RSpec support

--- lua/ror/navigators/test.lua
+++ lua/ror/navigators/test.lua
@@ -1,73 +1,95 @@
+local utils = require("ror.navigators.utils")
+
 local M = {}
 
-function M.visit(mode)
-  local current_relative_file_path = vim.fn.expand("%:~:.")
+local function detect_test_framework()
+  if M._framework then
+    return M._framework
+  end
 
-  if string.match(current_relative_file_path, "app/models") then
-    local model_name = vim.fn.fnamemodify(current_relative_file_path, ":t:r")
-    local test_name = model_name .. "_test.rb"
+  local root_path = vim.fn.getcwd()
+  if vim.fn.isdirectory(root_path .. "/spec") == 1 then
+    M._framework = { dir = "spec", suffix = "_spec" }
+  else
+    M._framework = { dir = "test", suffix = "_test" }
+  end
 
-    local tests = vim.split(vim.fn.system({ "find", "test/models", "-name", test_name }), "\n")
-    local parsed_tests = {}
-    for _, test in pairs(tests) do
-      if test ~= "" then
-        table.insert(parsed_tests, test)
-      end
-    end
+  return M._framework
+end
 
-    if #parsed_tests > 1 then
-      require("ror.picker").pick("Model Tests", parsed_tests)
-    elseif #parsed_tests == 1 then
-      if mode == "normal" then
-        vim.cmd.edit(parsed_tests[1])
-      elseif mode == "vsplit" then
-        vim.cmd.vsplit(parsed_tests[1])
-      end
+local function source_to_test(current_path, mode)
+  local fw = detect_test_framework()
+
+  local subpath, name
+  local start, finish = string.find(current_path, "app/")
+  if start then
+    subpath = string.sub(current_path, finish + 1)
+  else
+    start, finish = string.find(current_path, "lib/")
+    if start then
+      subpath = string.sub(current_path, finish + 1)
     else
-      local nvim_notify_ok, nvim_notify = pcall(require, 'notify')
-      if nvim_notify_ok then
-        nvim_notify(
-          "No test with name: " .. test_name,
-          vim.log.levels.ERROR,
-          { title = "Test file not found", timeout = 2500 }
-        )
-      else
-        vim.notify("Test file not found")
-      end
-    end
-  elseif string.match(current_relative_file_path, "app/controllers") then
-    local controller_name = vim.fn.fnamemodify(current_relative_file_path, ":t:r")
-    local test_name = controller_name .. "_test.rb"
-
-    local tests = vim.split(vim.fn.system({ "find", "test/controllers", "-name", test_name }), "\n")
-    local parsed_tests = {}
-    for _, test in pairs(tests) do
-      if test ~= "" then
-        table.insert(parsed_tests, test)
-      end
-    end
-
-    if #parsed_tests > 1 then
-      require("ror.picker").pick("Controller Tests", parsed_tests)
-    elseif #parsed_tests == 1 then
-      if mode == "normal" then
-        vim.cmd.edit(parsed_tests[1])
-      elseif mode == "vsplit" then
-        vim.cmd.vsplit(parsed_tests[1])
-      end
-    else
-      local nvim_notify_ok, nvim_notify = pcall(require, 'notify')
-      if nvim_notify_ok then
-        nvim_notify(
-          "No test with name: " .. test_name,
-          vim.log.levels.ERROR,
-          { title = "Test file not found", timeout = 2500 }
-        )
-      else
-        vim.notify("Test file not found")
-      end
+      return false
     end
   end
+
+  name = vim.fn.fnamemodify(subpath, ":t:r")
+  local dir_part = vim.fn.fnamemodify(subpath, ":h")
+  local test_name = name .. fw.suffix .. ".rb"
+  local search_dir = fw.dir .. "/" .. dir_part
+
+  if vim.fn.isdirectory(search_dir) ~= 1 then
+    return false
+  end
+
+  local files = utils.find_files(search_dir, test_name)
+  utils.open_or_pick("Tests", files, mode, "No test found: " .. test_name)
+  return true
+end
+
+local function test_to_source(current_path, mode)
+  local fw = detect_test_framework()
+
+  local prefix = fw.dir .. "/"
+  local start, _ = string.find(current_path, prefix)
+  if start ~= 1 then
+    return false
+  end
+
+  local subpath = string.sub(current_path, #prefix + 1)
+  local name = vim.fn.fnamemodify(subpath, ":t:r")
+  local dir_part = vim.fn.fnamemodify(subpath, ":h")
+
+  local source_name = string.gsub(name, fw.suffix .. "$", "")
+  local source_file = source_name .. ".rb"
+  local source_dir = "app/" .. dir_part
+
+  local files = utils.find_files(source_dir, source_file)
+
+  if #files == 0 and string.find(dir_part, "^lib") then
+    source_dir = dir_part
+    files = utils.find_files(source_dir, source_file)
+  elseif #files == 0 then
+    files = utils.find_files("lib/" .. dir_part, source_file)
+  end
+
+  utils.open_or_pick("Source", files, mode, "No source file found: " .. source_file)
+  return true
+end
+
+function M.visit(mode)
+  local current_path = vim.fn.expand("%:~:.")
+  local fw = detect_test_framework()
+
+  if string.find(current_path, "^" .. fw.dir .. "/") then
+    test_to_source(current_path, mode)
+  else
+    source_to_test(current_path, mode)
+  end
+end
+
+function M.reset_framework_cache()
+  M._framework = nil
 end
 
 return M
